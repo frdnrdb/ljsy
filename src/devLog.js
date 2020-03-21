b80e0b2064b64b9673d78647c14e4a5bd775e2ba
frode nordbo <frode.nordbo@fvn.no>
2020-03-21 22:18:23 +0100

    devLog

--- src/devLog.js
+++ src/devLog.js
@@ -1,19 +1,18 @@
-export default (() => {
-    const _debugElement = document.getElementById('dev-debugger') || (div => {
-        div.id = 'dev-debugger';
-        div.style.position = 'fixed';
-        div.style.top = '1em';
-        div.style.left = '1em';
-        div.style.background = '#000';
-        div.style.padding = '.5em';
-        div.style.color = '#fff';
-        div.style.fontFamily = 'sans-serif';
-        return div;
-    })(document.body.appendChild(document.createElement('div'))); 
+const _debugElement = () => document.getElementById('dev-debugger') || (div => {
+    div.id = 'dev-debugger';
+    div.style.position = 'fixed';
+    div.style.top = '1em';
+    div.style.left = '1em';
+    div.style.background = '#000';
+    div.style.padding = '.5em';
+    div.style.color = '#fff';
+    div.style.fontFamily = 'sans-serif';
+    return div;
+})(document.body.appendChild(document.createElement('div'))); 
 
-    return (...args) => {
-        const append = (typeof args[0] === 'boolean') && args.shift();
-        if (!append) _debugElement.innerHTML = '';
-        _debugElement.innerHTML += args.map(JSON.stringify).join(' ');
-    };
-})()
+export default (...args) => {
+    const element = _debugElement();
+    const append = (typeof args[0] === 'boolean') && args.shift();
+    if (!append) element.innerHTML = '';
+    element.innerHTML += args.map(JSON.stringify).join(' ');
+};
